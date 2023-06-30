92856d70e3223738fab3aa39632106868a67d0b5
emrecanortakci <ortakciemrecan@gmail.com>
2023-06-30 16:10:54 +0300

    config-server added

--- config-server/src/test/java/com/inventorymanagement/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/inventorymanagement/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.inventorymanagement.productservice;
+package com.inventorymanagement.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductServiceApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
